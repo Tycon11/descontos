8c61071e923d3f5d8762cb6acdc648e269e7b6c6
Tyc <114003770+Tycon11@users.noreply.github.com>
2026-09-14 15:51:18 -0300

    Delete Desconto directory

--- Desconto/script.js
+++
@@ -1,8 +0,0 @@
-function Desconto() {
-    let preco = parseFloat(document.getElementById("Preco").value);
-    let desconto = parseFloat(document.getElementById("pa").value);
-    let valorDesconto = preco * (desconto / 100);
-    let precoFinal = preco - valorDesconto;
-    let Nome = document.getElementById("Nome").value;
-    alert("Produto '" + Nome + "' com " + desconto + "% de desconto: R$ " + precoFinal.toFixed(2));
-}
